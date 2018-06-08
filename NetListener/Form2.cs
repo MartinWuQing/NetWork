a4b7b2ba71be856de9167665943707dd3a3af736
mhq <martin_mahuaqun@163.com>
2018-06-08 16:40:58 +0800

    将网速显示到窗体上。

---
+++ NetListener/Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using WHU.Commons;
+using WindowsTest.Business;
+
+namespace NetListener
+{
+    public partial class Form2 : Form
+    {
+
+        private delegate void ThreadWork(int quality);
+
+        string ServerIP = "";
+        int StardardBandwidth = 1024;
+        Thread NetworkChecker = null;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            NetworkChecker = new Thread(new ThreadStart(SystemMonitoring));
+
+            NetworkChecker.IsBackground = true;
+            NetworkChecker.Start();
+            AppConfig ac = new AppConfig();
+            ServerIP = ac.AppConfigGet("ServerIP");
+            StardardBandwidth = int.Parse(ac.AppConfigGet("StardardBandwidth"));
+            int quality = NetworkCheck();
+
+            label1.Text = quality.ToString();
+
+        }
+
+        private void SystemMonitoring()
+        {
+            Thread.Sleep(1000);
+            while (true)
+            {
+
+                //关键，返回网络质量数据
+                int quality = NetworkCheck();
+
+                UpdateMonitor(quality);
+
+                Thread.Sleep(5000);
+            }
+        }
+        private int NetworkCheck()
+            {
+                int result = 0;
+                float nw = Ping.Test(ServerIP);
+                result = (int)(nw / StardardBandwidth * 100);//以作为标准带宽，后期可调。
+                result %= 100;
+                return result;
+
+            }
+        //更新显示器
+        private void UpdateMonitor(int quality)
+        {
+            if (this.label1.InvokeRequired)//等待异步 
+            { 
+            
+                ThreadWork fc = new ThreadWork(UpdateMonitor);
+                // this.Invoke(fc);//通过代理调用刷新方法 
+                this.Invoke(fc, new object[1] { quality });
+            }
+            else
+            {
+                label1.Text = quality.ToString();
+                if (quality > 80)
+                {
+                    label1.ForeColor = Color.Green;
+                }
+                else if (quality > 60)
+                {
+                    label1.ForeColor = Color.Orange;
+                }
+                else if (quality > 40)
+                {
+                    label1.ForeColor = Color.Yellow;
+                }
+                else
+                {
+                    label1.ForeColor = Color.Red;
+                }
+            }
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private bool site = false;       //开始、停止状态
+        private Thread tdWork = null;    //工作线程
+        Process ps = null;               //工作进程
+
+        private delegate void DShowTime(int value);    //用于夸线程操作控件，方法委托
+
+        private int outCount = 4, show = 0;
+
+        protected override void OnLoad(EventArgs e)
+        {
+
+            base.OnLoad(e);
+        }
+
+        #region //线程函数-即主工作方法
+        private void AppRunMethod()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("Ping.exe"," www.baidu.com "+ " -t -l 1");
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+            psi.CreateNoWindow = true;
+
+            ps = Process.Start(psi);
+
+            string outStream = ""; //储存Ping.exe输出流字符串
+            int time = 0;
+
+            while (site)
+            {
+                outStream = ps.StandardOutput.ReadLine();
+                time = GetTime(outStream);
+
+                //通过委托来调用ShowTime方法
+                //之所以要用委托是因为不能夸线程直接对控件进行操作
+                //所以需要通过委托，调用方法进行操作
+                Invoke(new DShowTime(ShowTime), time);
+                Thread.Sleep(500);
+                //强行执行垃圾回收，避免循环造成的垃圾堆积内存
+                GC.Collect();
+            }
+        }
+        #endregion
+
+        #region //从返回流字符串中截取出延迟
+        private int GetTime(string line)
+        {
+            int result = 0;
+            if (line == null || string.Empty == line.Trim())
+                return 0;
+            Regex rg = new Regex(@".*\=(.*\d+)ms.*");
+            int.TryParse(rg.Match(line).Groups[1].Value, out result);
+            if (result == 0)
+                ++outCount;
+            else
+                outCount = 0;
+            return result;
+        }
+        #endregion
+
+        #region //显示网速
+        private void ShowTime(int value)
+        {
+            if (value > 0)
+                show = value;
+            if (outCount <= 3 && show != 0)
+            {
+                label2.Text = show.ToString() + " ms";
+
+            }
+            else
+            {
+                label2.Text = "未连接";
+
+            }
+          }
+        #endregion
+
+        //停止按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (site)
+            {
+                site = false;
+                StopListent();
+
+
+                //停止监听，重置相关属性及状态
+                show = 0;
+                label2.Text = "未连接";
+
+            }
+        }
+
+        private void StopListent()
+        {
+            if (ps != null)
+            {
+                //如果进程存在就将其杀掉
+                if (!ps.HasExited)
+                    ps.Kill();
+                //如果线程仍在运行，强制停止
+                if (tdWork.IsAlive)
+                    tdWork.Abort();               
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!site)
+            {
+                //当site为false时，则创建一个新的线程，并在新线程中执行“线程函数”
+                site = true;
+                tdWork = new Thread(new ThreadStart(AppRunMethod));
+                tdWork.Start();
+                ChangeSite();  //将开始按钮置为不可用，地址框不可写
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
